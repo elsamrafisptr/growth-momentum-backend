09cfcdf90fe9c7fddb3a210757135ec66e20ba61
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2024-10-16 19:03:20 +0700

    feat(General): initialize project

---
+++ src/controllers/course.py
@@ -0,0 +1,14 @@
+from flask_restful import Resource
+
+class CourseController(Resource):
+    def get():
+        return ""
+    
+    def post():
+        return ""
+    
+    def put():
+        return ""
+    
+    def delete():
+        return ""
---
+++ src/controllers/user.py
@@ -0,0 +1,14 @@
+from flask_restful import Resource
+
+class UserController(Resource):
+    def get():
+        return ""
+    
+    def post():
+        return ""
+    
+    def put():
+        return ""
+    
+    def delete():
+        return ""
