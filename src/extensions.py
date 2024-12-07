0489107aac7c1ed24925c1d4d4e9af4e15702a0f
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2024-12-07 16:02:21 +0700

    add dockerfile and setup requirements

--- src/extensions.py
+++ src/extensions.py
@@ -3,11 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from flask_mysqldb import MySQL
 
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
 cors = CORS()
 bcrypt = Bcrypt()
-mysql = MySQL()
